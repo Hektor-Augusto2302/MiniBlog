903fbdee6d4df52c4cff5337f89ea650ee954ad1
Hektor-Augusto <hektoraugusto.s@gmail.com>
2024-02-01 10:10:09 -0300

    feat(new firebase configuration)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,17 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD2ifNK_czMZlYm_qWTx_yeUAirGf4weMA",
-    authDomain: "miniblog-15fe4.firebaseapp.com",
-    projectId: "miniblog-15fe4",
-    storageBucket: "miniblog-15fe4.appspot.com",
-    messagingSenderId: "709490831751",
-    appId: "1:709490831751:web:7883196d6bf95dadd5e322",
-    measurementId: "G-VXM1PCJWZR"
+    apiKey: "AIzaSyB9fqlY2dnxw9qrNtLJezopRF1lnI0HImo",
+    authDomain: "miniblog-b90f1.firebaseapp.com",
+    projectId: "miniblog-b90f1",
+    storageBucket: "miniblog-b90f1.appspot.com",
+    messagingSenderId: "386230759555",
+    appId: "1:386230759555:web:5d46d175366c8862d66bb6",
+    measurementId: "G-2BTR4GZQ29"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-export {db};
+export { db };
